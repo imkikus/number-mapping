860b41d01dad9abf9881cd786f1578ab6d51d7f5
imkikus <imkikus@gmail.com>
2019-01-02 06:58:45 +0530

    Group the combination of words based on the input

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,18 +20,43 @@ class WelcomeController < ApplicationController
 
       word_hash = {}
 
-      for i in (1..15)
+      for i in (1..20)
         word_hash[i] = []
       end
 
       file = File.join(Rails.root, 'lib', 'assets', 'dictionary.txt')
-      dictionary = File.read(file)
-
       File.foreach(file) do |word|
-        dictionary[word.length] << word.downcase.chop.to_s
+        word_hash[word.length] << word.downcase.chop.to_s
       end
 
-      @dictionary = dictionary
+      mapped_keys = phone_number.chars.map do |digit|
+        num_hash[digit]
+      end
+
+      grouped_hash = {}
+      number_of_digits = mapped_keys.length - 1
+
+      for i in (2..number_of_digits - 2)
+        array1 = mapped_keys[0..i]
+        if array1.length < 3
+          next
+        end
+        array2 = mapped_keys[i + 1..number_of_digits]
+        if array2.length < 3
+          next
+        end
+        combo1 = array1.shift.product(*array1).map(&:join)
+        if combo1.nil?
+          next
+        end
+        combo2 = array2.shift.product(*array2).map(&:join)
+        if combo2.nil?
+          next
+        end
+        grouped_hash[i] = [(combo1 & word_hash[i+2]), (combo2 & word_hash[number_of_digits - i +1])]
+      end
+
+      @grouped_hash = grouped_hash
 
     else
       flash[:info] = "Please enter the phone number"
