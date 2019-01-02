a6ad3bd09939fb0c3c938c064c47f5ad667732d6
imkikus <imkikus@gmail.com>
2019-01-02 06:16:08 +0530

    Read contents from the dictionary

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,40 @@
 class WelcomeController < ApplicationController
   def index
     if(params[:q])
-      @phone_number = params[:q]
+      phone_number = params[:q]
+
+      if phone_number.nil? || phone_number.length != 10
+        return Array.new
+      end
+
+      num_hash = {
+        '2' => ['a','b','c'],
+        '3' => ['d','e','f'],
+        '4' => ['g','h','i'],
+        '5' => ['j','k','l'],
+        '6' => ['m','n','o'],
+        '7' => ['p','q','r','s'],
+        '8' => ['t','u','v'],
+        '9' => ['w','x','y','z']
+      }
+
+      word_hash = {}
+
+      for i in (1..15)
+        word_hash[i] = []
+      end
+
+      file = File.join(Rails.root, 'lib', 'assets', 'dictionary.txt')
+      dictionary = File.read(file)
+
+      File.foreach(file) do |word|
+        dictionary[word.length] << word.downcase.chop.to_s
+      end
+
+      @dictionary = dictionary
+
+    else
+      flash[:info] = "Please enter the phone number"
     end
   end
 end
